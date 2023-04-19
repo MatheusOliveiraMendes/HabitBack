c59cd56f3692f664ce32a725ceeb12cc94196635
Matheus Mendes <matheusmendes@Laptop-de-Matheus.local>
2023-04-19 15:32:26 -0300

    update host

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
+
 import { appRoutes } from "./routes";
 
 const app = Fastify()
 
-app.register(cors)
+app.register(cors, {
+    origin: true,
+})
 app.register(appRoutes)
 
 app.listen({
-    port: 3333
-}) .then(() => {
-    console.log('HTTP Server running!')
+    port: 3333,
+    host: '0.0.0.0',
+}) .then((url) => {
+    console.log(`HTTP Server running on ${url}!`)
 })
